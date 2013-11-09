b8eaee4f106ccfe1ec0c3766cec38623b86b7249
Benjamin Weaver <bweavgolfanatic@aol.com>
2013-11-09 13:10:20 -0500

    initial commit created empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Step1::Application.config.secret_token = '8257b09b14bb81c5795b0d8fddd8c034859bbdc3c9039249a6ef4796afd193f9b0cffd24806ebfddfb30f208bd6e231a1f08f9fa9b3da19bddc2e7361c1a77aa'
