48b64bf9d83948ee03c6b0ce797b39391d9218c3
Benjamin Weaver <bweavgolfanatic@aol.com>
2013-12-15 15:25:43 -0500

    tes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -113,6 +113,7 @@ class PostsController < ActionController::Base
 
   def user_posts
     j_posts = Hash.new
+    puts params[:username]
     @posts = Post.where("user_id = ?", User.find_by_username(params[:username]).id).find_each do |post|
       j_posts[post.id] = post.title
     end
