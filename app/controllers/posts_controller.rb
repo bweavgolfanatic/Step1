150d102ca75a6b9c0d83ab620bb401ad8de3eba9
Benjamin Weaver <bweavgolfanatic@aol.com>
2013-12-14 19:15:16 -0500

    by title

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ActionController::Base
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:title])
     respond_to do |format|
       
       format.json { render json: @post }
